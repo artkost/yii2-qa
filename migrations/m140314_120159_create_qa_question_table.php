bbe074391c0a923da83fdfa014056198eff81ee6
Nikolay Kostyurin <jilizart@gmail.com>
2014-03-14 21:10:35 +0700

    migrations

---
+++ migrations/m140314_120159_create_qa_question_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Schema;
+
+class m140314_120159_create_qa_question_table extends \yii\db\Migration
+{
+	public function up()
+	{
+
+	}
+
+	public function down()
+	{
+		echo "m140314_120159_create_qa_question_table cannot be reverted.\n";
+		return false;
+	}
+}
---
+++ migrations/m140314_120441_create_qa_answer_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Schema;
+
+class m140314_120441_create_qa_answer_table extends \yii\db\Migration
+{
+	public function up()
+	{
+
+	}
+
+	public function down()
+	{
+		echo "m140314_120441_create_qa_answer_table cannot be reverted.\n";
+		return false;
+	}
+}

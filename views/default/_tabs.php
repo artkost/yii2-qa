867581dd48480e21eb6cbae6ae7456906796ccff
Nikolay Kostyurin <jilizart@gmail.com>
2014-05-20 03:10:03 +0700

    remove short tags

--- views/default/_tabs.php
+++ views/default/_tabs.php
@@ -8,9 +8,9 @@ use artkost\qa\Module;
     <a class="qa-index-add-button btn btn-primary" href="<?= Module::url(['ask']) ?>"><?= Module::t('Ask a Question') ?></a>
     <ul class="qa-index-tabs nav nav-tabs">
         <li <?= ($route == 'index') ? 'class="active"':''?>><a href="<?= Module::url(['index']) ?>"><?= Module::t('Active') ?></a></li>
-        <? if(!Yii::$app->user->isGuest): ?>
+        <?php if(!Yii::$app->user->isGuest): ?>
             <li <?= ($route == 'my') ? 'class="active"':''?>><a href="<?= Module::url(['my']) ?>"><?= Module::t('My') ?></a></li>
             <li <?= ($route == 'favorite') ? 'class="active"':''?>><a href="<?= Module::url(['favorite']) ?>"><?= Module::t('Favorite') ?></a></li>
-        <? endif; ?>
+        <?php endif; ?>
     </ul>
 </div>
